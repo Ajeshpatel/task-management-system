7880f858273f8df8047088fde0f13b87733643ae
Ajesh Patel <142934056+Ajeshpatel@users.noreply.github.com>
2025-06-10 09:42:21 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:8080/api/users/login",
+        "https://task-management-system-xx1b.onrender.com/api/users/login",
         form
       );
 
